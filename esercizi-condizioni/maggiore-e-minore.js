ea76fc874bcf2a1e0227f1cb32a0d24a55dbc77f
Francesco <71519347+FrancescoRapisarda@users.noreply.github.com>
2020-09-21 21:37:04 +0200

    Update maggiore-e-minore.js

--- esercizi-condizioni/maggiore-e-minore.js
+++ esercizi-condizioni/maggiore-e-minore.js
@@ -10,3 +10,44 @@
 
   http://www.imparareaprogrammare.it
 */
+
+
+//START
+
+
+var a = 3;
+var b = -1;
+var c = 4;
+var d = -2;
+
+var max;
+var min;
+
+if (a > b) {
+  max = a;
+  min = b;
+} else {
+  min = a;
+  max = b;
+}
+if (c > max) {
+  max = c;
+  min = min;
+} else {
+  min = c;
+  max = max;
+}
+if (d > max) {
+  max = d;
+  min = min;
+} else {
+  min = d;
+  max = max;
+}
+
+console.log("Il numero maggiore è: " + max);
+console.log("Il numero minore è: " + min);
+
+
+
+//END
