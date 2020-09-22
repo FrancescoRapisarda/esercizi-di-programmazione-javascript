945333a04d3c2ebe7e93326434128233c6df620b
Francesco <71519347+FrancescoRapisarda@users.noreply.github.com>
2020-09-22 21:29:19 +0200

    Update menu-dei-dolci.js

--- esercizi-condizioni/menu-dei-dolci.js
+++ esercizi-condizioni/menu-dei-dolci.js
@@ -21,3 +21,49 @@
 
   http://www.imparareaprogrammare.it
 */
+
+
+//START
+
+var scelta1 = 1;
+var scelta2 = 2;
+var scelta3 = 5; //3
+var scelta4 = 4;
+
+
+console.log("MENU' DEL GIORNO:");
+console.log("1. Tiramisù;");
+console.log("2. Torta della Nonna;");
+console.log("3. Cheesecake alla Nutella;");
+console.log("4. Macedonia");
+
+console.log("");
+
+console.log("IL TUO MENU':");
+
+if (scelta1 == 1) {
+  console.log("Hai scelto il Tiramisù.");
+} else {
+  console.log("Dolce non disponibile nel menù di oggi!");
+}
+
+if (scelta2 == 2) {
+  console.log("Hai scelto la Torta della Nonna.");
+} else {
+  console.log("Dolce non disponibile nel menù di oggi!");
+}
+
+if (scelta3 == 3) {
+  console.log("Hai scelto il Cheesecake alla Nutella.");
+} else {
+  console.log("Dolce non disponibile nel menù di oggi!");
+}
+
+if (scelta4 == 4) {
+  console.log("Hai scelto la Macedonia.");
+} else {
+  console.log("Dolce non disponibile nel menù di oggi!");
+}
+
+
+//END
