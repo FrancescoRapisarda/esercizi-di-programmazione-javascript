38ea61389b1064656713829e9c2fb48b2ab76baa
Francesco <71519347+FrancescoRapisarda@users.noreply.github.com>
2020-09-23 10:25:25 +0200

    Update il-conta-cifre.js

--- esercizi-condizioni/il-conta-cifre.js
+++ esercizi-condizioni/il-conta-cifre.js
@@ -11,3 +11,32 @@
 
   http://www.imparareaprogrammare.it
 */
+
+
+
+//START
+
+const n = 9999;
+
+
+if (n > 0 && n < 10) {
+  console.log(n + " è un numero a 1 cifra!")
+} else if (n > 9 && n < 100) {
+  console.log(n + " è un numero a 2 cifre!");
+} else if (n > 99 && n < 1000) {
+  console.log(n + " è un numero a 3 cifre!");
+} else if (n > 999 && n < 10000) {
+  console.log(n + " è un numero a 4 cifre!");
+} else {
+  console.log("Ehi! Sembra che tu abbia scritto un numero troppo alto!");
+  console.log("Cifra max consentita: 9999.");
+}
+
+
+//END
+
+
+/* PS.
+Grazie Alberto per gli esercizi che hai messo gratuitamente fruibili, 
+mi sto davvero divertendo e sto imparando a ragionare sul problema, cosa che prima non sapevo fare!
+Grazie ancora, ti seguo molto volentieri, le tue informazioni e le tue risorse sono molto preziose, continua cosi.. */
