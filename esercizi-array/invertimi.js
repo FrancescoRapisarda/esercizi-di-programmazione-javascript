cf225a0cb4bbbb243518155f5813fcd37db46dec
Francesco <71519347+FrancescoRapisarda@users.noreply.github.com>
2020-09-26 09:25:18 +0200

    Update invertimi.js

--- esercizi-array/invertimi.js
+++ esercizi-array/invertimi.js
@@ -15,3 +15,24 @@
 
   http://www.imparareaprogrammare.it
 */
+
+
+//START
+
+
+var n = 5;
+var array = [0,1,2,3,4];
+
+for (var i = 0; i<n; i++) {
+array[0] = Math.floor(Math.random() * 10) + 1;
+array[1] = Math.floor(Math.random() * 10) + 1;
+array[2] = Math.floor(Math.random() * 10) + 1;
+array[3] = Math.floor(Math.random() * 10) + 1;
+array[4] = Math.floor(Math.random() * 10) + 1;
+}
+
+console.log(array);
+console.log(array.reverse());
+
+
+//END
