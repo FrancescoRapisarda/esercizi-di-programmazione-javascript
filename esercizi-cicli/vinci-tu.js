30828831304aa22a9c77e408b84c03d35a0ea3d4
Francesco <71519347+FrancescoRapisarda@users.noreply.github.com>
2020-10-06 08:25:43 +0200

    Update vinci-tu.js

--- esercizi-cicli/vinci-tu.js
+++ esercizi-cicli/vinci-tu.js
@@ -14,3 +14,49 @@
 
   http://www.imparareaprogrammare.it
 */
+
+
+//START
+
+
+var player1 = [0,1,2];
+var player2 = [0,1,2];
+var totPlayer1;
+var totPlayer2;
+
+  player1[0] = Math.floor(Math.random()*(7-1)+1);
+  player1[1] = Math.floor(Math.random()*(7-1)+1);
+  player1[2] = Math.floor(Math.random()*(7-1)+1);
+
+totPlayer1 = player1[0] + player1[1] + player1[2];
+console.log("Player1!");
+console.log("Dado1: " + player1[0] + "  Dado2: " + player1[1] + "  Dado3: " + player1[2]);
+console.log("Punteggio Giocatore Uno: " + totPlayer1);
+
+
+  player2[0] = Math.floor(Math.random()*(7-1)+1);
+  player2[1] = Math.floor(Math.random()*(7-1)+1);
+  player2[2] = Math.floor(Math.random()*(7-1)+1);
+
+totPlayer2 = player2[0] + player2[1] + player2[2];
+console.log(" ");
+console.log("Player2!");
+console.log("Dado1: " + player2[0] + "  Dado2: " + player2[1] + "  Dado3: " + player2[2]);
+console.log("Punteggio Giocatore Due: " + totPlayer2);
+
+
+if (totPlayer1 > totPlayer2) {
+    console.log(" ");
+    console.log("Giocatore Uno: hai vinto tu!");
+}
+else if (totPlayer1 == totPlayer2) {
+  console.log(" ");
+  console.log("Giocatore Uno e Giocatore Due: avete ottenuto lo stesso punteggio!");
+}
+else {
+    console.log(" ");
+    console.log("Giocatore Due: hai vinto tu!");
+}
+
+
+//END
